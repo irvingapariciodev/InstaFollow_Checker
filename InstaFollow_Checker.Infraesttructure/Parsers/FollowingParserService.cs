6d54a207d9fe4ebdac5b6d73fe3c81fc37a479f6
irvingapa12 <90082635+irvingapa12@users.noreply.github.com>
2026-03-28 20:13:06 -0600

    add dependency injection configuration for infrastructure services

---
+++ InstaFollow_Checker.Infraesttructure/Parsers/FollowingParserService.cs
@@ -0,0 +1,23 @@
+﻿using InstaFollow_Checker.Application.Interfaces;
+using System.Text.Json;
+
+namespace InstaFollow_Checker.Infraesttructure.Parsers
+{
+    internal class FollowingParserService : IGetFollowing
+    {
+        public IEnumerable<string> GetFollowing(string jsonContent)
+        {
+            using var document = JsonDocument.Parse(jsonContent);
+
+            return document.RootElement
+                .GetProperty("string_list_data")
+                .EnumerateArray()
+                .Select(x => x.GetProperty("href").GetString())
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(ExtractUsername)!;
+        }
+
+        private static string ExtractUsername(string url)
+            => url.Split('/').Last();
+    }
+}

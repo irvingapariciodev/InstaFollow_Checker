35e1004414d65f5f5c6d3c8cb4bbf57a2d414cf7
irvingapa12 <90082635+irvingapa12@users.noreply.github.com>
2026-04-03 17:14:18 -0600

    - Create design for projects page - Create design for InstaFollow Checker - Create form that allows to upload the followers.json and following.json files so that I can start the analysis

---
+++ InstaFollow_Checker.Web/Pages/Projects/InstaFollowChecker/InstaFollowChecker.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InstaFollow_Checker.Web.Pages.Projects.InstaFollowChecker
+{
+    public class InstaFollowCheckerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
